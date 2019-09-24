0975fe2beae7063331e8f400fe8f7299184f8c8b
Benjamin Fagin <blouis@unquietcode.com>
2019-09-23 23:04:04 -0700

    #16 better metadata

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setuptools.setup(
     name="runbook",
     version="0.1",
@@ -12,7 +15,10 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/UnquietCode/runbook.py",
+    keywords="runbook playbook process",
     packages=setuptools.find_namespace_packages(exclude=['test']),
+    install_requires=requirements,
+    license='OSI Approved :: Apache Software License'
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
